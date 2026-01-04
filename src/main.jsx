b83cea4effd2118037937ef140b84d6460eb8644
Muhamad Abdelmawla <mohamedelkalb221@gmail.com>
2026-01-04 22:36:45 +0200

    new project

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import './App.scss';
 import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 )
