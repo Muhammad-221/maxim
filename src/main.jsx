a350785e3c66dee94a192353f36786c9afb8380a
Muhamad Abdelmawla <mohamedelkalb221@gmail.com>
2026-01-05 16:45:48 +0200

    new project

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.css";
 import './App.scss';
 
